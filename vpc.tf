bd4b7fa035558544f1084b8afdaa80514538a9ef
jesus <jesussoro@hotmail.com>
2021-11-05 13:10:09 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,2 @@
-network_name = gaurav-network
-project_id = var.project
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+#network_name = gaurav-network
+#project_id = var.project
